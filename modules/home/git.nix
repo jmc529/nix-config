dd182a3c2bd358062aeb600d94072f17900775e8
Joe <jmc529@users.noreply.github.com>
2026-08-27 13:58:03 -0400

    feat: hardware config/core changes

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,7 +4,7 @@
   programs.git = {
     enable = true;
     userName = "jmc529";
-    userEmail = "jmconwell54@gmail.com";
+    userEmail = "jmc529@users.noreply.github.com";
 
     extraConfig = {
       init.defaultBranch = "main";
--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -5,4 +5,17 @@
     enable = true;
     plugins = [ pkgs.networkmanager-openvpn ];
   };
+
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices
+      Experimental = true;
+      ControllerMode = "bredr";
+    };
+  };
+
+  services.printing.enable = true;
 }
