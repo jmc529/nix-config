25f6216132ab39b2668eafa8b601d19ca19547dd
Joe <jmc529@users.noreply.github.com>
2026-08-27 17:28:16 -0400

    typo fix

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,14 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
   users.users.joe = {
     isNormalUser = true;
     description = "Joe";
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     shell = pkgs.zsh;
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
   };
 }
