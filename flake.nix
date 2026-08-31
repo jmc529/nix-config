d46d98fc5a33e4c4f18782fa8c4b3a0486c76b60
Joe <jmc529@users.noreply.github.com>
2026-08-31 18:02:45 -0400

    fix: pin home-manager

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
