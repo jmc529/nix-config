bf644293e3adf472f47f43894df8b95efe77453f
Joe <jmc529@users.noreply.github.com>
2026-09-05 17:42:32 -0400

    Feat: new nix apps (#6)
    * feat: new nix apps
    
    * fix: build warnings
    
    * fix: build warnings

--- modules/home/apps.nix
+++ modules/home/apps.nix
@@ -19,5 +19,17 @@
 
     # Management
     bitwarden-desktop
+
+    # nix mcp (AI model context protocal) gives info on nix https://github.com/utensils/mcp-nixos
+    mcp-nixos
+    
+    # nix doc format https://github.com/NixOS/nixfmt
+    nixfmt
+
+    # update nix pkgs https://github.com/Mic92/nix-update
+    nix-update
+
+    # https://nixsoftware.org/en/packages/devtoolbox?channel=2605
+    devtoolbox
   ];
 }
